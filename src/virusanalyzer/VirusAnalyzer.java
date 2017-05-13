387778440bf8bc707ec7fbc56f8e285120f17ea4
ThejanRupasinghe <thejan94@gmail.com>
2017-05-13 23:53:05 +0530

    All done

--- src/virusanalyzer/VirusAnalyzer.java
+++ src/virusanalyzer/VirusAnalyzer.java
@@ -7,7 +7,9 @@ package virusanalyzer;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -23,19 +25,37 @@ public class VirusAnalyzer {
         try {
             System.out.println("Select the file to detect for virus ");
             
+            //File chooser select the file to detect
             File file = fileChooser();
-            String file_name = file.getName();
-                        
-//            String text = fh.fileReader(textFile.toString());
-            AnalyzingLogic al = new AnalyzingLogic();
-            al.md5Generator(file.toString());
+            System.out.println(file.toString());
+
+            AnalyzingLogic logic = new AnalyzingLogic();
+            
+            //Generating md5 checksum value of the given file
+            String fileChecksum = logic.md5Generator(file.toString());
+            
+            //Reading the virus definitions and storing them in a array list
+            FileHandler fh = new FileHandler();
+            ArrayList<String> virusDefinitions = fh.readVirusDefinition();
+            
+            //Taking the result of the analysis
+            boolean isVirus = logic.analyze(fileChecksum, virusDefinitions);
+            
+            if(isVirus){
+                System.out.println("Virus Detected !!");
+                JOptionPane.showMessageDialog(null, file.getName()+" file contains a virus.\nFile MD5: "+fileChecksum+"\nis found in virus definitions", "Virus Detected !!",JOptionPane.WARNING_MESSAGE);
+            }else{
+                System.out.println("Clean File !!");
+                JOptionPane.showMessageDialog(null, file.getName()+" file doesnot contain a virus.\nFile MD5: "+fileChecksum+"\nis not found in virus definitions", "No Virus !!",JOptionPane.INFORMATION_MESSAGE);
+            }
          
         } catch (Exception ex) {
             System.out.println("IO Exception");
         }
     }
     
-        public static File fileChooser(){
+    //File chooser to select file
+    public static File fileChooser(){
         JFileChooser chooser = new JFileChooser();
         chooser.setDialogTitle("Select the file to detect");
 
